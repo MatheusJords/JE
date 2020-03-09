71386a2ae290064ed78f0ca2cf02a5621a1f8410
MatheusJords <matheus_jordan@yahoo.com.br>
2020-03-09 13:51:40 -0300

    Adicionando galeria

--- JE/instalacao.php
+++ JE/instalacao.php
@@ -31,7 +31,61 @@
 			</div>
 			<div class="row">
 				<div class="col-md">
-					<img src="img/servico1.jpg" style="height:200px;width:200px;margin-bottom:30px;"/>
+				
+					<!-- Modal -->
+					<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+						<div class="modal-dialog modal-dialog-centered" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalCenterTitle">Projeto 1</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+								<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+									<ol class="carousel-indicators">
+										<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+										<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+									</ol>
+									<div class="carousel-inner">
+										<div class="carousel-item active">
+											<img src="img/servico1.jpg" style="height:500px;width:100%;">
+										</div>
+										<div class="carousel-item">
+											<img src="img/servico1.jpg" style="height:500px;width:100%;">
+										</div>
+										<div class="carousel-item">
+											<img src="img/servico1.jpg" style="height:500px;width:100%;">
+										</div>
+										<div class="carousel-item">
+											<img src="img/servico1.jpg" style="height:500px;width:100%;">
+										</div>
+									</div>
+									<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+										<span class="sr-only">Previous</span>
+									</a>
+									<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+										<span class="carousel-control-next-icon" aria-hidden="true"></span>
+										<span class="sr-only">Next</span>
+									</a>
+								</div>
+									
+								</div>
+								<div class="modal-footer">
+								
+								</div>
+							</div>	
+						</div>
+					</div>
+					<img src="img/servico1.jpg" style="height:200px;width:200px;margin-bottom:10px;"/>
+					<br>
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+						Album
+					</button>
 				</div>
 				<div class="col-md">
 					<img src="img/servico2.jpg" style="height:200px;width:200px;margin-bottom:30px;"/>
